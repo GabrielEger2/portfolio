cce511463399e22d3281f1cffad792a1de12757d
Gabriel Eger <52424334+GabrielEger2@users.noreply.github.com>
2024-04-15 22:13:03 -0300

    feat: add footer

---
+++ src/content/layout/FooterContent.ts
@@ -0,0 +1,28 @@
+export const footerLinks = [
+  {
+    title: 'Website Sections',
+    links: [
+      { title: 'Home', href: '/' },
+      { title: 'About Me', href: '/about' },
+      { title: 'Projects', href: '/projects' },
+      { title: 'Education', href: '/education' },
+    ],
+  },
+  {
+    title: 'Technologies Used',
+    links: [
+      { title: 'Next.js', href: 'https://nextjs.org/' },
+      { title: 'Tailwind CSS', href: 'https://tailwindcss.com/' },
+      { title: 'TypeScript', href: 'https://www.typescriptlang.org/' },
+      { title: 'Docker', href: 'https://www.docker.com/' },
+    ],
+  },
+  {
+    title: 'Social Accounts',
+    links: [
+      { title: 'Linkedin', href: 'https://www.linkedin.com/in/gabrieleger' },
+      { title: 'GitHub', href: 'https://github.com/GabrielEger2' },
+      { title: 'LeetCode', href: 'https://leetcode.com/GabrielEger2' },
+    ],
+  },
+]
