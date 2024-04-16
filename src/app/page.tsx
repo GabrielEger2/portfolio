dd9d112bbf5b925fd1226bcfa6e3610eb36c6572
Gabriel Eger <52424334+GabrielEger2@users.noreply.github.com>
2024-04-15 21:31:13 -0300

    feat: add initial project configuration

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const home = () => {
+  return (
+    <div>
+      <div role="alert" className="alert">
+        <span>12 unread messages. Tap to see.</span>
+      </div>
+    </div>
+  )
+}
+
+export default home
