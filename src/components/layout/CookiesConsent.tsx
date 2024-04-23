04eeb9273ae0abe41f0cbfe133df622fbc62b2f7
Gabriel Eger <52424334+GabrielEger2@users.noreply.github.com>
2024-04-22 22:13:17 -0300

    feat: add in-out cookieConsent animation

--- src/components/layout/CookiesConsent.tsx
+++ src/components/layout/CookiesConsent.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { AnimatePresence, motion } from 'framer-motion'
+
 import Cookies from 'js-cookie'
 import { useEffect, useState } from 'react'
 import { BiX } from 'react-icons/bi'
@@ -27,27 +29,39 @@ const CookiesConsent = () => {
 
   return (
     <div className="fixed bottom-4 z-50 w-full flex">
-      {showCookieBanner && (
-        <div className="alert shadow-lg max-w-96 bg-base-200">
-          <div>
-            <div className="flex justify-between">
-              <h3 className="font-bold">Your Experience Matters to Me!</h3>
-              <BiX className="cursor-pointer" size={24} onClick={handleDeny} />
+      <AnimatePresence>
+        {showCookieBanner && (
+          <motion.div
+            className="alert shadow-lg max-w-96 bg-base-200"
+            initial={{ y: 100, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: 100, opacity: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            <div>
+              <div className="flex justify-between">
+                <h3 className="font-bold">Your Experience Matters to Me!</h3>
+                <BiX
+                  className="cursor-pointer"
+                  size={24}
+                  onClick={handleDeny}
+                />
+              </div>
+              <div className="flex justify-between space-x-4 mt-4">
+                <p className="text-sm text-start">
+                  Accept the use of cookies for a better experience
+                </p>
+                <button
+                  onClick={handleAccept}
+                  className="btn btn-success text-base-100"
+                >
+                  Accept
+                </button>
+              </div>
             </div>
-            <div className="flex justify-between space-x-4 mt-4">
-              <p className="text-sm text-start">
-                Accept the use of cookies for a better experience
-              </p>
-              <button
-                onClick={handleAccept}
-                className="btn btn-success text-base-100"
-              >
-                Accept
-              </button>
-            </div>
-          </div>
-        </div>
-      )}
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   )
 }
