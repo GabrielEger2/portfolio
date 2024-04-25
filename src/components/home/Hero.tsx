3979aa5845ba66926891a6a34fa4f5721a97fa47
Gabriel Eger <52424334+GabrielEger2@users.noreply.github.com>
2024-04-25 14:34:59 -0300

    feat: add hero mountains

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -9,7 +9,43 @@ const Hero = () => {
   return (
     <>
       <div className="min-h-screen overflow-hidden relative ">
-        <div className="flex h-[70vh] items-end">
+        <div className="flex flex-col h-[70vh] justify-end w-screen">
+          <div className="w-full">
+            <svg
+              id="visual"
+              viewBox="0 0 1920 1080"
+              width="100%"
+              height="auto"
+              xmlns="http://www.w3.org/2000/svg"
+              version="1.1"
+            >
+              <path
+                d="M0 829L213 881L427 780L640 884L853 829L1067 743L1280 745L1493 888L1707 763L1920 847L1920 1081L1707 1081L1493 1081L1280 1081L1067 1081L853 1081L640 1081L427 1081L213 1081L0 1081Z"
+                fill="oklch(var(--p))"
+                fillOpacity={0.4}
+              />
+              <path
+                d="M0 934L213 881L427 780L640 884L853 829L1067 922L1280 895L1493 910L1707 875L1920 861L1920 1081L1707 1081L1493 1081L1280 1081L1067 1081L853 1081L640 1081L427 1081L213 1081L0 1081Z"
+                fill="oklch(var(--p))"
+                fillOpacity={0.4}
+              />
+              <path
+                d="M0 934L213 891L427 1001L640 941L853 982L1067 1006L1280 951L1493 989L1707 996L1920 959L1920 1081L1707 1081L1493 1081L1280 1081L1067 1081L853 1081L640 1081L427 1081L213 1081L0 1081Z"
+                fill="oklch(var(--p))"
+                fillOpacity={0.5}
+              />
+              <path
+                d="M0 970L213 962L427 1001L640 894L853 982L1067 922L1280 905L1493 973L1707 912L1920 959L1920 1081L1707 1081L1493 1081L1280 1081L1067 1081L853 1081L640 1081L427 1081L213 1081L0 1081Z"
+                fill="oklch(var(--p))"
+                fillOpacity={0.7}
+              />
+              <path
+                d="M0 983L213 997L427 1024L640 1014L853 968L1067 970L1280 1048L1493 1024L1707 983L1920 959L1920 1081L1707 1081L1493 1081L1280 1081L1067 1081L853 1081L640 1081L427 1081L213 1081L0 1081Z"
+                fill="oklch(var(--p))"
+                fillOpacity={1}
+              />
+            </svg>
+          </div>
           <div className="w-full h-[0.025rem] bg-primary tranlate-y-1" />
         </div>
         <div className="h-[30vh] w-full">
