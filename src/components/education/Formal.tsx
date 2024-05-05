a6af3e50ec0bd309eee9b99ae3e70a6082d1eafc
Gabriel Eger <52424334+GabrielEger2@users.noreply.github.com>
2024-05-05 15:21:03 -0300

    feat: add formal education timeline

--- src/components/education/Formal.tsx
+++ src/components/education/Formal.tsx
@@ -1,23 +1,80 @@
+import { BiSolidCheckCircle } from 'react-icons/bi'
+
 const Formal = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <div className="mx-auto grid max-w-5xl grid-cols-1 gap-8 px-4 md:grid-cols-12">
-        <h2 className="col-span-1 text-3xl font-bold md:col-span-4">
-          Formal Education
-        </h2>
-        <div className="col-span-1 md:col-span-8">
-          <p className="mb-4 text-xl md:text-2xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi,
-            blanditiis soluta eius quam modi aliquam quaerat odit deleniti
-            minima maiores voluptate est ut saepe accusantium maxime doloremque
-            nulla consectetur possimus.
-          </p>
-          <p className="mb-8 text-xl md:text-2xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
-            reiciendis blanditiis aliquam aut fugit sint.
-          </p>
-          <button className="btn btn-primary">Learn more</button>
-        </div>
+    <div className="flex flex-col md:flex-row w-full min-h-screen items-center justify-between">
+      <div className="w-full md:w-1/2 flex justify-center">
+        <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
+          <li>
+            <div className="timeline-start md:text-end mb-10" />
+            <hr className="bg-primary" />
+          </li>
+          <li>
+            <hr className="bg-primary" />
+            <div className="timeline-middle">
+              <BiSolidCheckCircle size={20} />
+            </div>
+            <div className="timeline-start mb-10">
+              <time className="font-mono italic text-sm">2017 - 2021</time>
+              <h1 className="font-black">Technical High School</h1>
+              <h3>IFSC (Federal Institute of Santa Catarina)</h3>
+              <p className="italic text-sm">GPA: 8.9 - 10</p>
+            </div>
+            <hr className="bg-primary" />
+          </li>
+          <li>
+            <hr className="bg-primary" />
+            <div className="timeline-middle">
+              <BiSolidCheckCircle size={20} />
+            </div>
+            <div className="timeline-end mb-10">
+              <time className="font-mono italic text-sm">
+                2023 - 2026 (Probably)
+              </time>
+              <h1 className="font-black">Bsc Computer Science</h1>
+              <h3>GoldSmith - University of London</h3>
+              <p className="italic text-sm">GPA: ????</p>
+            </div>
+            <hr />
+          </li>
+          <li>
+            <hr />
+            <div className="timeline-middle">
+              <BiSolidCheckCircle size={20} />
+            </div>
+            <div className="timeline-start mb-10">
+              <time className="font-mono italic text-sm">
+                2027 - 2028 (Probably)
+              </time>
+              <h1 className="font-black">Master in Computer Science</h1>
+              <h3>????</h3>
+              <p className="italic text-sm">GPA: ????</p>
+            </div>
+            <hr />
+          </li>
+          <li>
+            <hr />
+            <div className="timeline-middle">
+              <BiSolidCheckCircle size={20} />
+            </div>
+            <div className="timeline-end mb-10">
+              <time className="font-mono italic text-sm">
+                2029 - 2031 (Probably)
+              </time>
+              <h1 className="font-black">Phd Computer Science</h1>
+              <h3>????</h3>
+              <p className="italic text-sm">GPA: ????</p>
+            </div>
+            <hr />
+          </li>
+          <li>
+            <hr />
+            <div className="timeline-start md:text-end mb-10" />
+          </li>
+        </ul>
+      </div>
+      <div className="w-full md:w-1/2 flex justify-center">
+        Add animated scroll soon
       </div>
     </div>
   )
