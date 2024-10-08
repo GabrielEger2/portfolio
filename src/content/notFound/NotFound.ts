018837b39630eb348f1d0d79dff0c3eaa85b1f30
Gabriel Eger <52424334+GabrielEger2@users.noreply.github.com>
2024-10-07 22:08:54 -0300

    feat: add notFound page

---
+++ src/content/notFound/NotFound.ts
@@ -0,0 +1,10 @@
+export const NotFoundTitle = '404 Not Found'
+
+export const NotFoundText =
+  'The page you are looking for, unfortunately, does not exist.'
+
+export const NotFoundButton = 'Go Back Home'
+
+export const HomeLink = '/'
+
+export const noiseImage = '/imgs/notFound/noise.jpg'
