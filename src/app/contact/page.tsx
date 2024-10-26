5bc8ad9b8e8041ec8a1ca1fa3318f7c393a75ee9
Gabriel Eger <52424334+GabrielEger2@users.noreply.github.com>
2024-10-26 17:04:47 -0300

    feat: add contact me content

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,68 +1,43 @@
-import { BiMailSend } from 'react-icons/bi'
+import {
+  contactFormEmail,
+  contactFormEmailPlaceholder,
+  contactFormMessage,
+  contactFormMessagePlaceholder,
+  contactFormName,
+  contactFormNamePlaceholder,
+  contactFormSubmit,
+  contactFormSurname,
+  contactFormSurnamePlaceholder,
+  contactText,
+  contactTitle,
+  contentHeader,
+  socialLinks,
+} from '@/content/contact/contact'
 
 const contact = () => {
   return (
     <div className="min-h-screen h-full pt-16 flex justify-center items-center">
       <div className="max-w-7xl w-full h-full px-2 flex gap-12">
         <div className="w-1/2">
-          <p className="font-medium text-primary">Contact Me</p>
+          <p className="font-medium text-primary">{contentHeader}</p>
           <h1 className="mt-2 text-2xl font-semibold md:text-3xl">
-            Chat to our friendly team
+            {contactTitle}
           </h1>
-          <p className="mt-3">
-            We’d love to hear from you. Please fill out this form or shoot us an
-            email.
-          </p>
+          <p className="mt-3">{contactText}</p>
           <div className="mt-12">
             <div className="grid grid-cols-1 gap-12 md:grid-cols-2">
-              <div>
-                <span className="inline-block p-3 text-primary-content rounded-full bg-primary">
-                  <BiMailSend size={20} />
-                </span>
-                <h2 className="mt-4 font-medium">Email</h2>
-                <p className="mt-2 text-sm">
-                  Our friendly team is here to help.
-                </p>
-                <p className="mt-2 text-sm text-blue-500 underline">
-                  gabriel_eger01@hotmail.com
-                </p>
-              </div>
-              <div>
-                <span className="inline-block p-3 text-primary-content rounded-full bg-primary">
-                  <BiMailSend size={20} />
-                </span>
-                <h2 className="mt-4 font-medium">Email</h2>
-                <p className="mt-2 text-sm">
-                  Our friendly team is here to help.
-                </p>
-                <p className="mt-2 text-sm text-blue-500 underline">
-                  gabriel_eger01@hotmail.com
-                </p>
-              </div>
-              <div>
-                <span className="inline-block p-3 text-primary-content rounded-full bg-primary">
-                  <BiMailSend size={20} />
-                </span>
-                <h2 className="mt-4 font-medium">Email</h2>
-                <p className="mt-2 text-sm">
-                  Our friendly team is here to help.
-                </p>
-                <p className="mt-2 text-sm text-blue-500 underline">
-                  gabriel_eger01@hotmail.com
-                </p>
-              </div>
-              <div>
-                <span className="inline-block p-3 text-primary-content rounded-full bg-primary">
-                  <BiMailSend size={20} />
-                </span>
-                <h2 className="mt-4 font-medium">Email</h2>
-                <p className="mt-2 text-sm">
-                  Our friendly team is here to help.
-                </p>
-                <p className="mt-2 text-sm text-blue-500 underline">
-                  gabriel_eger01@hotmail.com
-                </p>
-              </div>
+              {socialLinks.map((social, index) => (
+                <div key={index}>
+                  <span className="inline-block p-3 text-primary-content rounded-full bg-primary">
+                    {social.icon}
+                  </span>
+                  <h2 className="mt-4 font-medium">{social.title}</h2>
+                  <p className="mt-2 text-sm">{social.text}</p>
+                  <p className="mt-2 text-sm text-blue-500 underline">
+                    {social.link}
+                  </p>
+                </div>
+              ))}
             </div>
           </div>
         </div>
@@ -72,22 +47,22 @@ const contact = () => {
               <div className="flex gap-4 w-full">
                 <div className="form-control w-full">
                   <label className="label">
-                    <span className="label-text">Name:</span>
+                    <span className="label-text">{contactFormName}</span>
                   </label>
                   <input
                     type="text"
-                    placeholder="name"
+                    placeholder={contactFormNamePlaceholder}
                     className="input input-bordered"
                     required
                   />
                 </div>
                 <div className="form-control w-full">
                   <label className="label">
-                    <span className="label-text">Surname:</span>
+                    <span className="label-text">{contactFormSurname}</span>
                   </label>
                   <input
                     type="text"
-                    placeholder="surname"
+                    placeholder={contactFormSurnamePlaceholder}
                     className="input input-bordered"
                     required
                   />
@@ -95,26 +70,26 @@ const contact = () => {
               </div>
               <div className="form-control">
                 <label className="label">
-                  <span className="label-text">Email:</span>
+                  <span className="label-text">{contactFormEmail}</span>
                 </label>
                 <input
                   type="email"
-                  placeholder="example@email.com"
+                  placeholder={contactFormEmailPlaceholder}
                   className="input input-bordered"
                   required
                 />
               </div>
               <label className="form-control">
                 <div className="label">
-                  <span className="label-text">Message:</span>
+                  <span className="label-text">{contactFormMessage}</span>
                 </div>
                 <textarea
                   className="textarea textarea-bordered h-36"
-                  placeholder="I would like to know more about..."
+                  placeholder={contactFormMessagePlaceholder}
                 />
               </label>
               <div className="form-control mt-6">
-                <button className="btn btn-primary">Send E-mail</button>
+                <button className="btn btn-primary">{contactFormSubmit}</button>
               </div>
             </form>
           </div>
