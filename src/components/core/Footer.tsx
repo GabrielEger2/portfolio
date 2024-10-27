7c556401224bda4120f866cefce36c3e7d4deb92
Gabriel Eger <52424334+GabrielEger2@users.noreply.github.com>
2024-10-27 17:51:55 -0300

    feat: add footer content

--- src/components/core/Footer.tsx
+++ src/components/core/Footer.tsx
@@ -1,28 +1,45 @@
-import { footerContent, footerLinks } from '@/content/core/Footer'
+import { useTranslations } from 'next-intl';
 
 const Footer = () => {
+  const t = useTranslations('core.footer');
+
+  const footerLinksData = [
+    {
+      key: 'link1',
+      linksCount: 4
+    },
+    {
+      key: 'link2',
+      linksCount: 4
+    },
+    {
+      key: 'link3',
+      linksCount: 3
+    }
+  ];
+
   return (
     <div className="w-full flex justify-center bg-base-200">
       <footer className="footer p-10 text-base-content max-w-[88rem]">
         <aside>
           <p>
-            {footerContent.title}
+            {t('title')}
             <br />
-            {footerContent.description}
+            {t('description')}
           </p>
         </aside>
-        {footerLinks.map((section, index) => (
+        {footerLinksData.map(({ key, linksCount }, index) => (
           <nav key={index}>
-            <h6 className="footer-title">{section.title}</h6>
-            {section.links.map((link, index) => (
+            <h6 className="footer-title">{t(`footerLinks.${key}.title`)}</h6>
+            {Array.from({ length: linksCount }, (_, i) => (
               <a
-                key={index}
+                key={i}
                 className="link link-hover"
                 target="_blank"
                 rel="noopener noreferrer"
-                href={link.href}
+                href={t(`footerLinks.${key}.links.link${i + 1}.href`)}
               >
-                {link.title}
+                {t(`footerLinks.${key}.links.link${i + 1}.title`)}
               </a>
             ))}
           </nav>
--- src/components/projects/Hero.tsx
+++ src/components/projects/Hero.tsx
@@ -1,12 +1,15 @@
 'use client'
 
 import { motion, useScroll, useTransform } from 'framer-motion'
+import { useTranslations } from 'next-intl'
 import Link from 'next/link'
 import { useRef } from 'react'
 import { BiLink } from 'react-icons/bi'
 import ProjectsParallax from './ProjectsParallax'
 
 const Hero = () => {
+  const t = useTranslations('projects.parallaxCollumn.hero')
+
   const targetRef = useRef(null)
   const { scrollYProgress } = useScroll({
     target: targetRef,
@@ -28,24 +31,24 @@ const Hero = () => {
           style={{ opacity: opacityFadeOut, y: yFadeOut }}
           className="flex h-screen w-full flex-col items-start justify-end sticky top-0 pb-20"
         >
-          <p className="text-4xl font-bold md:text-7xl">
-            Explore All the Projects I&apos;ve Built so Far
-          </p>
+          <p className="text-4xl font-bold md:text-7xl">{t('title')}</p>
           <Link
-            href="https://github.com/GabrielEger2"
+            href={t('githubLink')}
             target="_blank"
             className="btn btn-lg text-xl font-bold btn-primary mt-4"
           >
-            GitHub Link <BiLink className="inline-block" size={30} />
+            {t('github')} <BiLink className="inline-block" size={30} />
           </Link>
         </motion.div>
         <motion.div
           style={{ opacity: opacityFadeIn }}
           className="flex h-screen w-full flex-col items-start justify-center sticky top-0 pb-4"
         >
-          <p className="text-4xl font-bold md:text-8xl">塵も積もれば山となる</p>
+          <p className="text-4xl font-bold md:text-8xl">
+            {t('japanesePhrase')}
+          </p>
           <p className="text-1xl font-semibold md:text-3xl mt-12">
-            - Even dust if piled can become a mountain
+            {t('japaneseTranslation')}
           </p>
         </motion.div>
       </div>
